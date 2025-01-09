913d0bcf8876709b9c51eab677033b2d08dce680
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2025-01-09 10:52:57 +0000

    chore: drop unused vitest options

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ mode }) => ({
   plugins: [tsconfigPaths()],
   test: {
-    testTimeout: 0,
-    env: loadEnv(mode, process.cwd(), ""),
     coverage: {
       reporter: ["text", "json-summary", "json"],
       reportOnFailure: true,
