ae13b8d965a8f9ba19316ab789dbf62358f9ccc5
Andrei Lapets <a@lapets.io>
2025-12-07 22:19:13 -0500

    ci: show coverage (ignoring configs); increase test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig((_config) => ({
   plugins: [tsconfigPaths()],
   test: {
+    testTimeout: 20000,
     coverage: {
+      enabled: true,
       reporter: ["text", "json-summary", "json", "lcov"],
       reportOnFailure: true,
+      exclude: ["./*.config.ts", "./bin/check-version.ts"],
     },
   },
 }));
